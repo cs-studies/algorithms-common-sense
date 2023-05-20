c092c8caac566d83b54e53101db73d9b07d0aecb
YugaEgo <yet@ego.team>
2023-05-20 20:14:39 +0300

    Adjust bubble_sort implementation

--- chapter_04/src/main.rs
+++ chapter_04/src/main.rs
@@ -4,17 +4,20 @@ mod exercises;
 fn main() {
     println!("\n*** Chapter 04 ***\n");
 
-    let v = vec![];
+    let mut v = vec![];
     println!("Vector: {:?}", v);
-    println!("Sorted: {:?}\n", bubble_sort(&v));
+    bubble_sort(&mut v);
+    println!("Sorted: {:?}\n", v);
 
-    let v = vec![4, 2, 7, 1, 3];
+    let mut v = vec![4, 2, 7, 1, 3];
     println!("Vector: {:?}", v);
-    println!("Sorted: {:?}\n", bubble_sort(&v));
+    bubble_sort(&mut v);
+    println!("Sorted: {:?}\n", v);
 
-    let v = vec![65, -55, 45, -35, 0, 15, 10];
+    let mut v = vec![65, -55, 45, -35, 0, 15, 10];
     println!("Vector: {:?}", v);
-    println!("Sorted: {:?}\n", bubble_sort(&v));
+    bubble_sort(&mut v);
+    println!("Sorted: {:?}\n", v);
 
     let a = [1, 4, 5, 2, 9];
     println!("Array: {:?}", a);
@@ -38,26 +41,24 @@ fn main() {
              exercises::greatest_number(&[5, 3, 7, 1]));
 }
 
-fn bubble_sort(data: &[i32]) -> Vec<i32> {
-    let mut result = data.to_owned();
-    let mut last_index = match result.len().checked_sub(1) {
+fn bubble_sort(data: &mut [i32]) {
+    let mut last_index = match data.len().checked_sub(1) {
         Some(val) => val,
-        None => return result,
+        None => return,
     };
     let mut is_sorted = false;
 
     while !is_sorted {
         is_sorted = true;
         for i in 0..last_index {
-            if result[i] > result[i + 1] {
+            if data[i] > data[i + 1] {
                 is_sorted = false;
-                result.swap(i, i + 1);
+                data.swap(i, i + 1);
             }
-            println!("Check index {i}: {:?}", result);
+            println!("Check index {i}: {:?}", data);
         }
         last_index -= 1;
     }
-    result
 }
 
 fn has_duplicates(data: &[i32]) -> bool {
@@ -94,10 +95,13 @@ mod tests {
 
     #[test]
     fn test_bubble_sort() {
-        assert_eq!(Vec::<i32>::new(), bubble_sort(&vec![]));
+        let mut v: Vec<i32> = vec![];
+        bubble_sort(&mut v);
+        assert_eq!(Vec::<i32>::new(), v);
 
-        let v = vec![65, -55, 45, -35, 0, 15, 10];
-        assert_eq!(vec![-55, -35, 0, 10, 15, 45, 65], bubble_sort(&v));
+        let mut a = [65, -55, 45, -35, 0, 15, 10];
+        bubble_sort(&mut a);
+        assert_eq!([-55, -35, 0, 10, 15, 45, 65], a);
     }
 
     #[test]
