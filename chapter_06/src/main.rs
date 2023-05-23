aac8db8c8cd1fd3beb606d7fcdb15a1aaa1d684d
YugaEgo <yet@ego.team>
2023-05-23 17:47:16 +0300

    Add intersection implementations

--- chapter_06/src/main.rs
+++ chapter_06/src/main.rs
@@ -5,6 +5,12 @@ fn main() {
     println!("Vector: {:?}", v);
     insertion_sort(&mut v);
     println!("Sorted: {:?}\n", v);
+
+    let m = [3, 1, 4, 2];
+    let n = [4, 5, 3, 6];
+    println!("m: {:?}", m);
+    println!("n: {:?}", n);
+    println!("Intersection: {:?}", intersection(&m, &n));
 }
 
 fn insertion_sort(data: &mut [i32]) {
@@ -15,6 +21,25 @@ fn insertion_sort(data: &mut [i32]) {
             p -= 1;
         }
     }
+}
+
+fn intersection(a: &[i32], b: &[i32]) -> Vec<i32> {
+    let mut result = vec![];
+    for i in a {
+        for j in b {
+            if i == j {
+                result.push(*i);
+                break;
+            }
+        }
+    }
+    result
+}
+
+//// Rust Extras
+#[allow(dead_code)]
+fn intersection_extra(a: &[i32], b: &[i32]) -> Vec<i32> {
+    a.iter().filter(|x| b.contains(x)).copied().collect()
 }
 
 #[cfg(test)]
@@ -30,5 +55,25 @@ mod tests {
         let mut a = [65, -55, 45, -35, 0, 15, 10];
         insertion_sort(&mut a);
         assert_eq!([-55, -35, 0, 10, 15, 45, 65], a);
+    }
+
+    #[test]
+    fn test_intersection() {
+        let v_empty = Vec::<i32>::new();
+        assert_eq!(v_empty, intersection(&[], &[]));
+        assert_eq!(v_empty, intersection(&[1], &[]));
+        assert_eq!(v_empty, intersection(&[], &[1]));
+        let x = intersection(&[3, 1, 4, 2], &[4, 5, 3, 6]);
+        assert_eq!(vec![3, 4], x);
+    }
+
+    #[test]
+    fn test_intersection_extra() {
+        let v_empty = Vec::<i32>::new();
+        assert_eq!(v_empty, intersection_extra(&[], &[]));
+        assert_eq!(v_empty, intersection_extra(&[1], &[]));
+        assert_eq!(v_empty, intersection_extra(&[], &[1]));
+        let x = intersection_extra(&[3, 1, 4, 2], &[4, 5, 3, 6]);
+        assert_eq!(vec![3, 4], x);
     }
 }
