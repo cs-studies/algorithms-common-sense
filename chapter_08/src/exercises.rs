e1c95174a4bf74e45578e40b81a3038525caddbc
YugaEgo <yet@ego.team>
2023-11-26 19:22:39 +0200

    Chapter 8: Improve exercise intersect()

--- chapter_08/src/exercises.rs
+++ chapter_08/src/exercises.rs
@@ -1,20 +1,14 @@
 use std::collections::HashSet;
 
 pub fn intersect(a: &[i32], b: &[i32]) -> Vec<i32> {
-    #[rustfmt::skip]
-    let (large, small) = if a.len() > b.len() {
-        (a, b)
-    } else {
-        (b, a)
-    };
-    let mut hs = HashSet::new();
-    for l in large {
-        hs.insert(l);
-    }
     let mut result = Vec::new();
-    for s in small {
-        if hs.contains(s) {
-            result.push(*s);
+    let mut hs = HashSet::new();
+    for i in a {
+        hs.insert(i);
+    }
+    for j in b {
+        if hs.contains(j) {
+            result.push(*j);
         }
     }
     result
@@ -31,6 +25,6 @@ mod tests {
         assert_eq!(intersect(&[1], &[]), v_empty);
         assert_eq!(intersect(&[], &[1]), v_empty);
         let x = intersect(&[3, 1, 4, 2], &[4, 5, 3, 6]);
-        assert_eq!(x, vec![3, 4]);
+        assert!(x.eq(&vec![3, 4]) || x.eq(&vec![4, 3]));
     }
 }
