d019f65f89671441ae9ae8e8ea0d8150a0c4d0d9
YugaEgo <yet@ego.team>
2024-03-10 18:21:12 +0200

    Chapter 13: improve quicksort implementation

--- chapter_13/src/main.rs
+++ chapter_13/src/main.rs
@@ -1,13 +1,16 @@
 fn main() {
     println!("\n*** Chapter 13 ***\n");
 
-    let mut v = vec![0, 5, 2, 1, 6, 3];
-    v.sort();
-    println!("sorted: {:?}", v);
+    let v = vec![0, -5, 2, 1, -6, 3];
+    println!("Before: {:?}", v);
 
-    // let sa = SortableArray::new(v);
+    let right_idx = v.len().saturating_sub(1);
+    let mut sa = SortableArray::new(v);
+    sa.quicksort(0, right_idx);
+    println!("After: {:?}", sa.data);
 }
 
+#[derive(Debug)]
 struct SortableArray {
     data: Vec<i32>,
 }
@@ -17,28 +20,56 @@ impl SortableArray {
         Self { data }
     }
 
-    fn partition(&mut self, mut left_idx: usize, mut right_idx: usize) -> usize {
-        let pivot_idx = right_idx;
-        let pivot = self.data[pivot_idx];
-        right_idx -= 1;
+    pub fn quicksort(&mut self, left: usize, right: usize) {
+        if left >= right {
+            return;
+        }
+        let pivot = self.partition(left, right);
+        if pivot > 0 {
+            self.quicksort(left, pivot - 1);
+        }
+        self.quicksort(pivot + 1, right);
+    }
+
+    fn partition(&mut self, mut left: usize, mut right: usize) -> usize {
+        let pivot = right;
+        let pivot_val = self.data[pivot];
+        right -= 1;
 
         loop {
-            while self.data[left_idx] < pivot {
-                left_idx += 1;
+            while self.data[left] < pivot_val {
+                left += 1;
             }
-            while self.data[right_idx] > pivot {
-                right_idx -= 1;
+            while self.data[right] > pivot_val {
+                if let Some(val) = right.checked_sub(1) {
+                    right = val;
+                } else {
+                    break;
+                }
             }
-            if left_idx >= right_idx {
+            if left >= right {
                 break;
             } else {
-                self.data.swap(left_idx, right_idx);
-                // left_idx += 1;
+                self.data.swap(left, right);
             }
         }
 
-        self.data.swap(left_idx, pivot_idx);
+        self.data.swap(left, pivot);
 
-        left_idx
+        left
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_quicksort() {
+        let data = vec![0, -5, 2, 6, 3];
+        let right_idx = data.len() - 1;
+        let mut sa = SortableArray::new(data);
+        sa.quicksort(0, right_idx);
+        assert_eq!(sa.data, [-5, 0, 2, 3, 6]);
     }
 }
