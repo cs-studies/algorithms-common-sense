73e16ebd91c91849698cbe9d8b3db0c5aa424925
YugaEgo <yet@ego.team>
2024-05-23 08:09:41 +0300

    Chapter 13: use reverse unstable sorting in exercise 1

--- chapter_13/src/exercises.rs
+++ chapter_13/src/exercises.rs
@@ -1,13 +1,11 @@
 pub fn greatest_product(data: &mut [u32]) -> Option<u32> {
-    let data_len = data.len();
-    if data_len < 3 {
+    if data.len() < 3 {
         return None;
     }
-    data.sort();
-
-    data[data_len - 1]
-        .checked_mul(data[data_len - 2])
-        .and_then(|p| p.checked_mul(data[data_len - 3]))
+    data.sort_unstable_by(|a, b| b.cmp(a));
+    data[0]
+        .checked_mul(data[1])
+        .and_then(|p| p.checked_mul(data[2]))
 }
 
 #[cfg(test)]
