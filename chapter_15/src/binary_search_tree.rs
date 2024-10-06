213b4d7707990b89bc1f97202b25da8d902e1bd5
YugaEgo <yet@ego.team>
2024-10-06 07:24:32 +0300

    Chapter 15: extract binary_search_tree module

---
+++ chapter_15/src/binary_search_tree.rs
@@ -0,0 +1,69 @@
+use std::cmp::Ordering;
+
+type Child<T> = Option<Box<TreeNode<T>>>;
+
+#[derive(Debug)]
+pub struct TreeNode<T> {
+    value: T,
+    left: Child<T>,
+    right: Child<T>,
+}
+
+impl<T: Ord> TreeNode<T> {
+    pub fn new(value: T, left: Child<T>, right: Child<T>) -> Self {
+        Self { value, left, right }
+    }
+
+    pub fn into_child(self) -> Child<T> {
+        Some(Box::new(self))
+    }
+
+    pub fn search(&self, value: T) -> Option<&Self> {
+        match value.cmp(&self.value) {
+            Ordering::Equal => Some(self),
+            Ordering::Less => self.left.as_deref()?.search(value),
+            Ordering::Greater => self.right.as_deref()?.search(value),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let node1 = TreeNode::new(11, None, None);
+        assert_eq!(node1.value, 11);
+        assert!(node1.left.is_none());
+        assert!(node1.right.is_none());
+
+        let node2 = TreeNode::new(12, node1.into_child(), None);
+        assert_eq!(node2.value, 12);
+        assert_eq!(node2.left.unwrap().value, 11);
+        assert!(node2.right.is_none());
+
+        let node3 = TreeNode::new(
+            13,
+            TreeNode::new(1, None, None).into_child(),
+            TreeNode::new(2, None, None).into_child(),
+        );
+        assert_eq!(node3.value, 13);
+        assert_eq!(node3.left.unwrap().value, 1);
+        assert_eq!(node3.right.unwrap().value, 2);
+    }
+
+    #[test]
+    fn test_search() {
+        let node1 = TreeNode::new(25, None, None);
+        assert_eq!(node1.search(25).unwrap().value, 25);
+
+        let node2 = TreeNode::new(
+            13,
+            TreeNode::new(1, None, None).into_child(),
+            TreeNode::new(22, None, None).into_child(),
+        );
+        assert!(node2.search(100).is_none());
+        assert_eq!(node2.search(22).unwrap().value, 22);
+    }
+}
--- chapter_15/src/main.rs
+++ chapter_15/src/main.rs
@@ -1,4 +1,6 @@
-use std::cmp::Ordering;
+mod binary_search_tree;
+
+use binary_search_tree::TreeNode;
 
 fn main() {
     println!("\n*** Chapter 15 ***\n");
@@ -13,72 +15,4 @@ fn main() {
 
     let found_none = root.search(80);
     dbg!(&found_none);
-}
-
-type Child<T> = Option<Box<TreeNode<T>>>;
-
-#[derive(Debug)]
-struct TreeNode<T> {
-    value: T,
-    left: Child<T>,
-    right: Child<T>,
-}
-
-impl<T: Ord> TreeNode<T> {
-    fn new(value: T, left: Child<T>, right: Child<T>) -> Self {
-        Self { value, left, right }
-    }
-
-    fn into_child(self) -> Child<T> {
-        Some(Box::new(self))
-    }
-
-    fn search(&self, value: T) -> Option<&Self> {
-        match value.cmp(&self.value) {
-            Ordering::Equal => Some(self),
-            Ordering::Less => self.left.as_deref()?.search(value),
-            Ordering::Greater => self.right.as_deref()?.search(value),
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_new() {
-        let node1 = TreeNode::new(11, None, None);
-        assert_eq!(node1.value, 11);
-        assert!(node1.left.is_none());
-        assert!(node1.right.is_none());
-
-        let node2 = TreeNode::new(12, node1.into_child(), None);
-        assert_eq!(node2.value, 12);
-        assert_eq!(node2.left.unwrap().value, 11);
-        assert!(node2.right.is_none());
-
-        let node3 = TreeNode::new(
-            13,
-            TreeNode::new(1, None, None).into_child(),
-            TreeNode::new(2, None, None).into_child(),
-        );
-        assert_eq!(node3.value, 13);
-        assert_eq!(node3.left.unwrap().value, 1);
-        assert_eq!(node3.right.unwrap().value, 2);
-    }
-
-    #[test]
-    fn test_search() {
-        let node1 = TreeNode::new(25, None, None);
-        assert_eq!(node1.search(25).unwrap().value, 25);
-
-        let node2 = TreeNode::new(
-            13,
-            TreeNode::new(1, None, None).into_child(),
-            TreeNode::new(22, None, None).into_child(),
-        );
-        assert!(node2.search(100).is_none());
-        assert_eq!(node2.search(22).unwrap().value, 22);
-    }
 }
