b1de7692ef873d9576ad940aa407c822b4778698
YugaEgo <yet@ego.team>
2024-11-14 18:22:42 +0200

    Chapter 17: add Trie and TrieNode structs

--- chapter_17/src/main.rs
+++ chapter_17/src/main.rs
@@ -1,3 +1,36 @@
+#![allow(unused_variables, dead_code, unused_imports)]
+
+use std::collections::HashMap;
+
 fn main() {
     println!("\n*** Chapter 17 ***\n");
+
+    let trie = Trie::<&str>::new();
+    dbg!(&trie);
+}
+
+#[derive(Debug)]
+struct Trie<T> {
+    root: TrieNode<T>
+}
+
+#[derive(Debug)]
+struct TrieNode<T> {
+    children: HashMap<T, TrieNode<T>>,
+}
+
+impl<T> Trie<T> {
+    fn new() -> Self {
+        Self {
+            root: TrieNode::new(),
+        }
+    }
+}
+
+impl<T> TrieNode<T> {
+    fn new() -> Self {
+        Self {
+            children: HashMap::new(),
+        }
+    }
 }
