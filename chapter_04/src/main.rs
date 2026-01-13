dc24bf8a2f13984f15b7941fe3968ccf5c9df2e4
YugaEgo <yet@ego.team>
2026-01-13 13:21:10 +0200

    Chapter 04: extend tests

--- chapter_04/src/main.rs
+++ chapter_04/src/main.rs
@@ -108,11 +108,13 @@ mod tests {
     fn test_has_duplicates() {
         assert_eq!(has_duplicates(&[]), false);
         assert!(has_duplicates(&[1, 5, 3, 9, 1, 4]));
+        assert!(!has_duplicates(&[1, 5, 3, 9, 4]));
     }
 
     #[test]
     fn test_has_duplicates_linear() {
         assert_eq!(has_duplicates_linear(&[]), false);
         assert!(has_duplicates_linear(&[1, 5, 3, 9, 1, 4]));
+        assert!(!has_duplicates_linear(&[1, 5, 3, 9, 4]));
     }
 }
